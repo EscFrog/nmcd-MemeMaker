9578807189c66d1a32079af84191a711d032246b
Walter Park <neodust223@gmail.com>
2023-04-16 20:10:31 +0900

    1.4 완료
    집 그리기

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.moveTo(50, 50);
-ctx.lineTo(150, 50);
-ctx.lineTo(150, 150);
-ctx.lineTo(50, 150);
-ctx.lineTo(50, 50);
-ctx.stroke();
+ctx.fillRect(200, 200, 50, 200);
+ctx.fillRect(400, 200, 50, 200);
+ctx.lineWidth = 2; // 이 구문을 strokeRect 뒤에 입력하면 이미 라인을 그린 후에 실행하기 때문에 적용이 되지 않는다.
+ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 200, 20);
+ctx.moveTo(200, 200);
+ctx.lineTo(325, 100);
+ctx.lineTo(450, 200);
+ctx.fill();
