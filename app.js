7fe5c79b9decc8502482bb3d014fb511d0731898
Walter Park <neodust223@gmail.com>
2023-04-19 23:38:35 +0900

    2.1 완료
    마우스 누른 채로 움직이면 마우스 움직임 따라서 선이 그어지도록 변경

--- app.js
+++ app.js
@@ -1,41 +1,31 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-let currentPos = { xPos: null, yPos: null };
-
 canvas.width = 800;
 canvas.height = 800;
 
 ctx.lineWidth = 2;
 
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-  "#18dcff",
-  "#7d5fff",
-];
+let isPainting = false;
 
-function drawLine(event) {
-  if (currentPos.xPos === null || currentPos.yPos === null) {
-    currentPos.xPos = event.offsetX;
-    currentPos.yPos = event.offsetY;
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
     return;
   }
-
-  ctx.beginPath();
-  ctx.moveTo(currentPos.xPos, currentPos.yPos);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
-
-  currentPos.xPos = event.offsetX;
-  currentPos.yPos = event.offsetY;
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", drawLine);
+function startPainting() {
+  isPainting = true;
+}
+
+function canclePainting() {
+  isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", canclePainting);
+canvas.addEventListener("mouseleave", canclePainting);
