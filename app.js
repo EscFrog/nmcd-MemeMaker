7feab2deb4dfe75c09ac65fe51d1f40c97b35a35
Walter Park <neodust223@gmail.com>
2023-04-23 23:28:36 +0900

    2.4 완료
    컬러 옵션을 추가해서 색을 선택할 수 있도록 함.
    주석에도 써 놨지만 getElementsByClassName() 함수가 반환하는 값은 HTML Collection이다. 이건 Array와 비슷하지만 Array가 아니기 때문에 forEach() 함수를 사용할 수 없다. 그러므로 Array.from() 함수를 이용해서 배열로 변환해줘야 forEach() 함수를 쓸 수 있다.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
-const colorPicker = document.getElementById("color");
+// forEach 구문은 자바스크립트 배열에만 쓸 수 있다. 그런데
+// 그런데 getElementsByClassName() 펑션이 리턴하는 것은 것은 HTML Collection이지 배열이 아니다.
+// 그래서 Array.from() 펑션을 이용해 배열로 만들어줘야 함.
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
+const colorPicker = document.getElementById("colorPicker");
 const lineThickness = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
@@ -9,6 +13,11 @@ canvas.height = 800;
 ctx.lineWidth = lineThickness.value;
 
 let isPainting = false;
+
+function changeColor(newColor) {
+  ctx.strokeStyle = newColor;
+  ctx.fillStyle = newColor;
+}
 
 function onMove(event) {
   if (isPainting) {
@@ -21,20 +30,25 @@ function onMove(event) {
 
 function startPainting() {
   isPainting = true;
+  ctx.beginPath();
 }
 
 function canclePainting() {
   isPainting = false;
-  ctx.beginPath();
 }
 
 function onLineWidthChange(event) {
   ctx.lineWidth = event.target.value;
 }
 
-function onColorChange(event) {
-  ctx.strokeStyle = event.target.value;
-  ctx.fillStyle = event.target.value;
+function colorPickerChange(event) {
+  changeColor(event.target.value);
+}
+
+function onColorOptionPick(event) {
+  let newColor = event.target.dataset.color;
+  changeColor(newColor);
+  colorPicker.value = newColor;
 }
 
 canvas.addEventListener("mousemove", onMove);
@@ -43,4 +57,8 @@ canvas.addEventListener("mouseup", canclePainting);
 canvas.addEventListener("mouseleave", canclePainting);
 
 lineThickness.addEventListener("change", onLineWidthChange);
-colorPicker.addEventListener("change", onColorChange);
+colorPicker.addEventListener("change", colorPickerChange);
+
+colorOption.forEach((color) =>
+  color.addEventListener("click", onColorOptionPick)
+);
