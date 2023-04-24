46e8deb95f4256f6f9f3f447ea66ecdfbc804d1c
Walter Park <neodust223@gmail.com>
2023-04-24 21:57:24 +0900

    2.6 완료
    clear 버튼을 누르면 하얀색으로 캔버스 전체를 칠하고, eraser 버튼을 누르면 strokeStyle을 하얀색으로 바꿔준다.

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const modeBtn = document.getElementById("mode-btn");
+const clearBtn = document.getElementById("clear-btn");
+const eraserBtn = document.getElementById("eraser-btn");
 // forEach 구문은 자바스크립트 배열에만 쓸 수 있다. 그런데
 // 그런데 getElementsByClassName() 펑션이 리턴하는 것은 것은 HTML Collection이지 배열이 아니다.
 // 그래서 Array.from() 펑션을 이용해 배열로 만들어줘야 함.
@@ -8,12 +10,15 @@ const lineThickness = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
 
 ctx.lineWidth = lineThickness.value;
 
-let isPainting = false;
+let isDrawing = false;
 let isFilling = false;
 
 function changeColor(newColor) {
@@ -22,7 +27,7 @@ function changeColor(newColor) {
 }
 
 function onMove(event) {
-  if (isPainting) {
+  if (isDrawing) {
     ctx.lineTo(event.offsetX, event.offsetY);
     ctx.stroke();
     return;
@@ -33,14 +38,14 @@ function onMove(event) {
 function startPainting() {
   ctx.beginPath();
   if (isFilling) {
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   } else {
-    isPainting = true;
+    isDrawing = true;
   }
 }
 
 function canclePainting() {
-  isPainting = false;
+  isDrawing = false;
 }
 
 function onLineWidthChange(event) {
@@ -68,6 +73,17 @@ function onModeClick() {
   }
 }
 
+function onClearClick() {
+  ctx.fillStyle = "white";
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function onEraseClick() {
+  ctx.strokeStyle = "white";
+  isFilling = false;
+  modeBtn.innerText = "Fill";
+}
+
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", canclePainting);
@@ -79,5 +95,6 @@ colorPicker.addEventListener("change", colorPickerChange);
 colorOption.forEach((color) =>
   color.addEventListener("click", onColorOptionPick)
 );
-
 modeBtn.addEventListener("click", onModeClick);
+clearBtn.addEventListener("click", onClearClick);
+eraserBtn.addEventListener("click", onEraseClick);
